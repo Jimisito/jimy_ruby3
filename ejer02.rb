5a381c785bc7cb302b1e1932b8184b36002b80f5
jimy <jimm.gom456@gmail.com>
2016-04-14 02:23:02 -0500

    segundo archivo:ejer02.rb

---
+++ ejer02.rb
@@ -0,0 +1,13 @@
+amigs=["jhonatan","magali","sheyla","analy","rimael"]
+puts "mi mejor amigo es: "+amigs[0]
+
+
+puts "================"
+
+
+amigs [0..4].each { |am| 
+        puts "mis amigs son: "+am
+ }
+
+
+gets()
